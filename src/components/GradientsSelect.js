3700511310822f0a9775357c201a2ab662b46aca
LeKarimDerradji <karim.derradji.pro@gmail.com>
2021-04-12 16:36:53 +0200

    gradient_select_useState

--- src/components/GradientsSelect.js
+++ src/components/GradientsSelect.js
@@ -1,4 +1,5 @@
 import reactDom from 'react-dom'
+import {useState} from 'react'
 import gradients from '../gradients'
 import  {uniqueTags} from '../gradients.js'
 
@@ -7,11 +8,14 @@ import  {uniqueTags} from '../gradients.js'
   // Todo créer le form GradientsSelect 
 
 const GradientsSelect = ({tag}) => {
-    
+    const [tagState, setTag] = useState('Tous');
+    const tagChange = (event) => {
+    setTag(event.target.value);
+  }
     return (
         <div class="input-group mb-3">
           <label class="input-group-text" for="select">Filtrer par tag</label>
-              <select class="form-select" id="select">
+              <select onChange={tagChange} class="form-select" id="select">
               <option value="all">{tag}</option>
               {uniqueTags.map((el) => (
                   <option key={el} value='all'>{el}</option>
